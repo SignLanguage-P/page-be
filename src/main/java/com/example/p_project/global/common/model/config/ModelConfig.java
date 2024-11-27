4ea7fb55e3c5d821b4e70a7001279c9a3d4fb887
Minch13r <0414minyoung@naver.com>
2024-11-27 22:52:33 +0900

    모델 설정 및 초기화

---
+++ src/main/java/com/example/p_project/global/common/model/config/ModelConfig.java
@@ -0,0 +1,4 @@
+package com.example.p_project.global.common.model.config;
+
+public class ModelConfig {
+}
---
+++ src/main/java/com/example/p_project/global/common/model/service/ModelInitializationService.java
@@ -0,0 +1,4 @@
+package com.example.p_project.global.common.model.service;
+
+public class ModelInitializationService {
+}
