1aacc52823b6c6f69676201913dceedb98daeb66
3333c <0414minyoung@naver.com>
2024-12-05 23:35:59 +0900

    Swagger 삽입 실행하고 http://localhost:8080/swagger-ui/index.html 들어가면 api 문서화 확인가능

--- src/main/java/com/example/p_project/global/config/SecurityConfig.java
+++ src/main/java/com/example/p_project/global/config/SecurityConfig.java
@@ -3,9 +3,12 @@ package com.example.p_project.global.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -14,5 +17,23 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf((csrf) -> csrf.disable())  // CSRF 보안 비활성화
+                .authorizeHttpRequests((authorize) -> authorize
+                        // Swagger UI 관련 경로 허용
+                        .requestMatchers(
+                                new AntPathRequestMatcher("/swagger-ui/**"),
+                                new AntPathRequestMatcher("/v3/api-docs/**"),
+                                new AntPathRequestMatcher("/swagger-resources/**")
+                        ).permitAll()
+                        // 기타 모든 요청은 인증 필요
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
     }
 }
